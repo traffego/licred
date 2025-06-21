aee47ac6e2ffb7e66b186e2a901c10f95d37f48c
traffego <traffego.mkt@gmail.com>
2025-06-21 10:48:20 -0300

    Licred - Subindo alterações finais - dados do banco 3

--- includes/conexao.php
+++ includes/conexao.php
@@ -5,15 +5,15 @@ ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/../logs/php_error.log');
 
 // Configurações do banco de dados
-// $host = "187.33.241.40";
-// $usuario = "platafo5_licred2";
-// $senha = "Licred2444#";
-// $banco = "platafo5_licred2";
+$host = "187.33.241.40";
+$usuario = "platafo5_licred3";
+$senha = "Licred2444#";
+$banco = "platafo5_licred3";
 
-$host = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "sistema_emprestimosv1_8";
+// $host = "localhost";
+// $usuario = "root";
+// $senha = "";
+// $banco = "sistema_emprestimosv1_8";
 
 try {
     // Criando a conexão
